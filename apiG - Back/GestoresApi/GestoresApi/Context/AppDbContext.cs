97f58a55ba38446fc702b37585ea44d254af4185
Tomas Nieto <81711363+nietotomas1645@users.noreply.github.com>
2021-11-27 18:21:56 +0100

    netcoreapi

---
+++ apiG - Back/GestoresApi/GestoresApi/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using GestoresApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestoresApi.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Gestores_Bd> gestores_bd { get; set; }
+
+    }
+}
---
+++ apiG - Back/GestoresApi/GestoresApi/Models/Gestores_Bd.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GestoresApi.Models
+{
+    public class Gestores_Bd
+    {   
+        [Key]
+        public int id { get; set; }
+
+        public string nombre { get; set; }
+
+        public int lanzamiento { get; set; }
+
+
+        public string desarrollador { get; set; }
+    }
+}
